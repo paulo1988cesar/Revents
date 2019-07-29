8177f1b304c971d59c3e56865a39603fa6413d24
César <paulo1988cesar@msn.com>
2019-07-29 18:35:06 -0300

    Finalizando Seção 5

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -11,6 +11,14 @@ class EventForm extends Component {
     hostedBy: ''
   }
 
+  componentDidMount() {
+    if(this.props.selectedEvent !== null) {
+      this.setState({
+        ...this.props.selectedEvent
+      })
+    }
+  } 
+
   handleInputChange = ({target: {name, value}}) => {
     this.setState({
       [name]: value
@@ -19,7 +27,11 @@ class EventForm extends Component {
 
   handleFormSubmit = (evt) => {
     evt.preventDefault();
-    this.props.createEvent(this.state);
+    if(this.state.id) {
+      this.props.updateEvent(this.state);
+    } else {
+      this.props.createEvent(this.state);
+    }
   }
 
   render() {
